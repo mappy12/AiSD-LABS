fb7ef1550f2a8e7d506d5cc2ea8db94c610a54b6
mappywin <mappydesuu@gmail.com>
2024-11-18 15:08:20 +0300

    added Node struct

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+template <typename T>
+class LinkedList {
+
+	template <typename T>
+	struct Node {
+		T data;
+		Node* next;
+		Node* prev;
+	};
+
+	Node<T>* head;
+	Node<T>* tail;
+	size_t size;
+
+public:
+
+	LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+	LinkedList(const LinkedList& other) {
+
+	}
+
+};
+
 int main() {
-	cout << "test" << endl;
+
 }
