4e77b0c2fb25931b174a3c271dadb5565bf15e0a
mappy-mac <mappydesuu@gmail.com>
2024-12-05 13:11:23 +0400

    added 'stats' struct

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct stats {
+    size_t comparisonCount = 0;
+    size_t copyCount = 0;
+};
+
 ostream& operator<<(ostream& os, vector<int> arr) {
 
     size_t size = arr.size();
@@ -16,23 +21,31 @@ ostream& operator<<(ostream& os, vector<int> arr) {
     return os;
 }
 
-vector<int> bubbleSort(vector<int> arr) {
+stats bubbleSort(vector<int>& arr) {
+
+    stats s;
 
     size_t size = arr.size();
 
     for (size_t i = 0; i < size; ++i) {
         for (size_t j = 0; j < size - i - 1; ++j) {
+
+            ++s.comparisonCount;
+
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
+                
+                s.copyCount += 3;
             }
         }
     }
 
-    return arr;
+    return s;
 }
 
-vector<int> shakeSort(vector<int> arr) {
+stats shakeSort(vector<int>& arr) {
 
+    stats s;
     size_t size = arr.size();
 
     int start = 0;
@@ -40,48 +53,87 @@ vector<int> shakeSort(vector<int> arr) {
     
     while (start <= end) {
         for (size_t i = start; i <= end - 1; ++i) {
+            ++s.comparisonCount;
+
             if (arr[i] > arr[i + 1]) {
                 swap(arr[i], arr[i + 1]);
+
+                s.copyCount += 3;
             }
         }
 
         ++start;
 
         for (size_t i = end; i >= start; --i) {
+            ++s.comparisonCount;
+
             if (arr[i] < arr[i - 1]) {
                 swap(arr[i], arr[i - 1]);
+
+                s.copyCount +=3;
             }
         }
 
         --end;
     }
 
-    return arr;
+    return s;
+
+}
+
+vector<int> quickSort(vector<int> arr) {
+    
+    size_t size = arr.size();
+
 
 }
 
 int main() {
 
-    vector<int> arr;
+    vector<int> arr1;
 
-    arr.push_back(10);
-    arr.push_back(20);
-    arr.push_back(135);
-    arr.push_back(30);
-    arr.push_back(53);
-    arr.push_back(91);
-    arr.push_back(53);
-    arr.push_back(19);
-    arr.push_back(77);
+    arr1.push_back(10);
+    arr1.push_back(20);
+    arr1.push_back(135);
+    arr1.push_back(30);
+    arr1.push_back(53);
+    arr1.push_back(91);
+    arr1.push_back(53);
+    arr1.push_back(19);
+    arr1.push_back(77);
 
-    cout << arr;   
+    cout << "----------------------------------\n";
+    cout << "Array_1: ";
+    cout << arr1;
+    cout << "----------------------------------\n\n";
 
-    vector<int> bsArr = bubbleSort(arr);
+    cout << "-----------------Bubble Sort-----------------\n\n";
+    stats bsStats = bubbleSort(arr1);
+    cout << arr1 << endl;
+    cout << "Comparison count: " << bsStats.comparisonCount << endl;
+    cout << "Copy count: " << bsStats.copyCount << endl << endl;
 
-    cout << bsArr;
+    vector<int> arr2;
+    arr2.push_back(43);
+    arr2.push_back(63);
+    arr2.push_back(1);
+    arr2.push_back(4);
+    arr2.push_back(7);
+    arr2.push_back(10);
+    arr2.push_back(28);
+    arr2.push_back(100);
+    arr2.push_back(99);
 
-    vector<int> ssArr = shakeSort(arr);
+    cout << "----------------------------------\n";
+    cout << "Array_2: ";
+    cout << arr2;
+    cout << "----------------------------------\n\n";
 
-    cout << ssArr;
+    cout << "-----------------Shake Sort-----------------\n\n";
+    stats ssStats = shakeSort(arr2);
+    cout << arr2 << endl;
+    cout << "Comparsion count: " << ssStats.comparisonCount << endl;
+    cout << "Copy count: " << ssStats.copyCount << endl << endl;
+    
 
 }
