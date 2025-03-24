dd2682a3ead9abccafee709ae31bc62108aeeb05
mappywin <mappydesuu@gmail.com>
2025-03-24 15:31:38 +0400

    added Constructor for an empty hash table of a given size

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,15 +1,22 @@
+constexpr size_t TABLE_SIZE = 32;
+
 
 template<typename T, typename K>
 class HashTable {
 
-	struct HashElm {
+	struct Item {
 		K key;
 		T value;
-		bool isEmpty
 
-		HashElm() : key(), value(), isEmpty(true) {}
+		Item() : key(), value() {}
+		Item(const key, const value) : key(key), value(value) {}
 	};
 
+	Item* elements;
+	size_t capacity = TABLE_SIZE;
+	size_t count = 0;
 
-
-};
+	HashTable(size_t capacity) : capacity(capacity) {
+		elements = new Item[capacity];
+	}
+}; 
