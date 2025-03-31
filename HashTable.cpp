90a1884df94f3f1239c77290ec632ae45502a0b5
mappywin <mappydesuu@gmail.com>
2025-03-31 19:07:49 +0400

    added copy constructor

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,9 +1,10 @@
 #include <cmath>
+#include <string>
 
-constexpr size_t TABLE_SIZE = 32;
+constexpr size_t TABLE_SIZE = 64;
 
 
-template<typename T, typename K>
+template<typename K, typename T>
 class HashTable {
 
 	struct Item {
@@ -14,17 +15,48 @@ class HashTable {
 		Item(const K key, const T value) : key(key), value(value) {}
 	};
 
+
 	Item* elements;
 	size_t capacity = TABLE_SIZE;
 	size_t count = 0;
+
+
+	size_t stringToInt(const std::string key) {
+
+		size_t hash = 0;
+
+		const size_t mod = 1e9 + 7;
+
+		for (char c : key) {
+
+			hash = (hash * 31 + c) % mod;
+
+		}
+
+		return hash;
+	}
+
 
 public:
 
 	HashTable(size_t capacity) : capacity(capacity) {
 
-		elements = new Item[capacity];
+		elements = new Item[capacity]();
 
 	}
+
+
+	HashTable(const HashTable& other) :
+		capacity(other.capacity),
+		count(other.count),
+		elements(new Item[other.capacity]) {
+
+		for (size_t i = 0; i < other.capacity; ++i) {
+			elements[i] = other.elements[i];
+		}
+
+	}
+
 
 	~HashTable() {
 
@@ -32,16 +64,18 @@ public:
 
 	}
 
-	int hash(long long key, size_t tableSize) {
 
-		double A = (sqrt(5) - 1) / 2;
+	size_t multiplicativeHash(T key) {
 
-		double x = key * A;
+		size_t a = 2654435769u;
+		
+		double x = (static_cast<double>(a) / 32) * key;
+		double fractPart = x - floor(x);
 
-		double fract_part = x - static_cast<int>(x);
-
-		return static_cast<int>(tableSize * fract_part);
+		return fractPart * capacity;
 
 	}
 
-}; 
+
+
+};
